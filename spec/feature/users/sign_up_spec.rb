8e12466d07665683dd6f9c077dbef696c00eee84
Oleg <nuda1234@bk.ru>
2019-05-08 18:42:20 +0300

    Hystories completed "sign out" and "sign up"

---
+++ spec/feature/users/sign_up_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+feature 'User can sign up', %q{
+  In order to ask questions
+  As an unauthenticated user
+  I'd like to be able to sign in
+} do
+
+  given(:user) { create(:user) }
+
+  background { visit new_user_registration_path }
+
+  scenario 'Unregistered user tries to sign up' do 
+    fill_in 'Email', with: 'new_user@mail'
+    fill_in 'Password', with: '111111'
+    fill_in 'Password confirmation', with: '111111'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'Unregistered user tries to sign up with errors' do
+    click_on 'Sign up'
+
+    expect(page).to have_content "Email can't be blank"
+  end
+
+  scenario 'Registered user try to sign up' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_on 'Sign up'
+
+    expect(page).to have_content "Email has already been taken"
+  end
+end
