3a421cecf5dbf920ba223139d80573a7d8d3a54e
Oleg <nuda1234@bk.ru>
2019-05-22 14:31:03 +0300

    6 ajax - Redact: model/answer_spec(set_best)

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Answer, type: :model do
   it { should belong_to(:author).class_name('User') }
 
   it { should validate_presence_of :body }
+
+  describe 'def set_best' do
+    let(:author) { create(:user) }
+    let(:question) { create(:question, author: author) }
+    let(:current_best_answer) { create(:answer, question: question, author: author) }
+    let(:answer) { create(:answer, question: question, author: author)}
+
+    before { answer.set_best }
+
+    it 'the answer is the best' do
+      expect(answer).to be_best
+    end
+
+    it 'there is only one best answer' do
+      current_best_answer.set_best
+      answer.reload
+      
+      expect(answer).to_not be_best
+      expect(current_best_answer).to be_best
+    end
+  end
 end
