b91487ba02229972ffe4021bf0f9c0d4fb65e1a7
Oleg <nuda1234@bk.ru>
2019-05-22 17:07:53 +0300

    7_active_storage - History: User can add one or more files when creating a response.

--- spec/feature/answers/create_spec.rb
+++ spec/feature/answers/create_spec.rb
@@ -15,11 +15,14 @@ feature 'User can create answer', %q{
 
     scenario 'write an answer', js: true  do
       fill_in 'Your answer', with: 'Answer Body Text'
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
       click_on 'Create'
 
       expect(current_path).to eq question_path(question)
       within '.answers' do
         expect(page).to have_content 'Answer Body Text'
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
       end
     end
 
