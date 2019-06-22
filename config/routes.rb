35209f608fa17fd86e82871956b0024d008b707a
Oleg <nuda1234@bk.ru>
2019-06-22 14:23:06 +0300

    11_omniauth - History: github

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'oauth_callbacks' }
   resources :attachments, only: :destroy
   resources :links, only: :destroy
   resources :rewards, only: :index
