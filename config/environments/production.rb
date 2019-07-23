cf154b2d2d330e0c45a0f084bcbe6e83aa6b67c2
Oleg <nuda1234@bk.ru>
2019-07-23 17:32:18 +0300

    add mail server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  #Mail config
+  config.action_mailer.default_url_options = { host: '68.183.3.73' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            Rails.application.credentials[Rails.env.to_sym][:email][:login],
+    password:             Rails.application.credentials[Rails.env.to_sym][:email][:password],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
