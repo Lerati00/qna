2a1c01b93b6ed86dbd34744652aca385b91768f7
Oleg <nuda1234@bk.ru>
2019-07-23 23:17:20 +0300

    change credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,3 @@
-Rails.application.routes.default_url_options[:protocol] = 'https'
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
