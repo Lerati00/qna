edd4c5cce3f360b08090c5b09dec10b777c0a312
Oleg <nuda1234@bk.ru>
2019-07-23 22:58:25 +0300

    change credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,10 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   #Mail config
-  config.action_mailer.default_url_options = { host: '68.183.3.73' }
+  config.action_mailer.default_url_options = {
+    host: '68.183.3.73',
+    protocol: 'https'
+  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
